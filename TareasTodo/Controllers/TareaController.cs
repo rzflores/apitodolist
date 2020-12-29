cacda9a4a42f562c3da6b1c8157d3ebdbc40bbb4
rflores <repaflocha@hotmail.com>
2020-12-28 21:14:21 -0500

    primer commit

---
+++ TareasTodo/Controllers/TareaController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TareasTodo.Models;
+
+namespace TareasTodo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        private  readonly ApplicationDbContext context;
+
+        public TareaController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Tarea> Get()
+        {
+            return context.Tareas.ToList();
+        }
+
+        [HttpGet("{id}" , Name = "tareaCreada")]
+        public IActionResult GetbyId(int id)
+        {
+            var tarea = context.Tareas.FirstOrDefault(item => item.Id == id);
+            
+            if (tarea == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(tarea);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Tareas.Add(tarea);
+                context.SaveChanges();
+                return new CreatedAtRouteResult("tareaCreada", new { id = tarea.Id },tarea);
+            }
+            return BadRequest(ModelState);
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Tarea tarea, int id) {
+            if (tarea.Id != id) 
+            {
+                return BadRequest();
+            
+            }
+            context.Entry(tarea).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok(tarea);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var tarea = context.Tareas.FirstOrDefault(item => item.Id == id);
+
+            if (tarea == null)
+            {
+                return NotFound();
+
+            }
+            context.Tareas.Remove(tarea);
+            context.SaveChanges();
+            return Ok(tarea);
+        }
+    }
+}
---
+++ TareasTodo/Models/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TareasTodo.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        [StringLength(10,ErrorMessage ="menos de 10 lineas")]
+        public String Descripcion { get; set; }
+
+        public bool Completada { get; set; }
+    }
+}
